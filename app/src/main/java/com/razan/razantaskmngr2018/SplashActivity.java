3e67e670dd36c429da69d797f4d9133dadc14634
user <rimaa@danon.tzafonet.org.il>
2018-10-10 08:27:09 +0300

    Initial commit

---
+++ app/src/main/java/com/razan/razantaskmngr2018/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.razan.razantaskmngr2018;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        MyThread myThread=new MyThread();
+        myThread.start();
+        super.onResume();
+    }
+
+
+    public class MyThread extends Thread
+    {
+        @Override
+        public void run() {
+            try {
+                sleep(3000);
+                Intent i=new Intent(getApplicationContext(),MainTabsActivity.class);
+                startActivity(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
