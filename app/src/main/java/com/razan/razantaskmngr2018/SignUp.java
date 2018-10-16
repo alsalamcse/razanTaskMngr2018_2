32b72f4011d6c9346faa08d1d7cad4ce13f49acd
user <rimaa@danon.tzafonet.org.il>
2018-10-16 13:22:29 +0300

    sign in / sign up

--- app/src/main/java/com/razan/razantaskmngr2018/SignUp.java
+++ app/src/main/java/com/razan/razantaskmngr2018/SignUp.java
@@ -1,17 +1,42 @@
 package com.razan.razantaskmngr2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignUp extends AppCompatActivity {
     private EditText etFirst,etLast,Phone,etEmail,etPassword;
-    Button btnSave;
+    private Button btnSave;
+    FirebaseUser user;// user
+    FirebaseAuth auth;// to establish sign in sign up
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        etFirst=findViewById(R.id.etFirst);
+        etLast=findViewById(R.id.etLast);
+        Phone=findViewById(R.id.phone);
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        btnSave=findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SignUp.this,LogInActivity.class);
+                startActivity(i);
+            }
+        });
+        auth=FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();//
     }
 }
