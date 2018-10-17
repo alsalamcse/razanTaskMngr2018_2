27c4cc86cd556c2c4b3369a8e1dc5916b491f4fd
user <rimaa@danon.tzafonet.org.il>
2018-10-17 08:56:37 +0300

    sign in / sign up

--- app/src/main/java/com/razan/razantaskmngr2018/SignUp.java
+++ app/src/main/java/com/razan/razantaskmngr2018/SignUp.java
@@ -1,20 +1,27 @@
 package com.razan.razantaskmngr2018;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
     private EditText etFirst,etLast,Phone,etEmail,etPassword;
     private Button btnSave;
-    FirebaseUser user;// user
-    FirebaseAuth auth;// to establish sign in sign up
+    //1.add of auth to project
+    //2.
+    private FirebaseUser user;// user
+    private FirebaseAuth auth;// to establish sign in sign up
 
 
 
@@ -36,7 +43,28 @@ public class SignUp extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        //3.
         auth=FirebaseAuth.getInstance();
         user=auth.getCurrentUser();//
+    }
+    //4.
+    private void creatAcount (String email,String passw){
+        auth.createUserWithEmailAndPassword(email, passw).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(SignUp.this, "Authemtication successful", Toast.LENGTH_SHORT).show();
+                    //updateUserProfil(task.getResult().getUser());
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(SignUp.this,"Authemtication failed."+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+
+                }
+            }
+        });
     }
 }
