1d3c36eaebe5b0c3d06753e9953293748064c615
user <rimaa@danon.tzafonet.org.il>
2018-12-03 13:05:35 +0200

    sign in / sign up

--- app/src/main/java/com/razan/razantaskmngr2018/SignUp.java
+++ app/src/main/java/com/razan/razantaskmngr2018/SignUp.java
@@ -101,9 +101,9 @@ public class SignUp extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(SignUp.this,LogInActivity.class);
-                startActivity(i);
-            }
+               Intent i = new Intent(SignUp.this,LogInActivity.class);
+               startActivity(i);
+           }
         });
     }
 }
