0b795e9be9e59fcd08a8fbe62d6b6e511a512dea
razan <razanh@danon.tzafonet.org.il>
2019-03-10 12:23:25 +0200

    shmerAH LLMO3TYAT

--- app/src/main/java/com/razan/razantaskmngr2018/ParkingActivity.java
+++ app/src/main/java/com/razan/razantaskmngr2018/ParkingActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,8 +17,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class ParkingActivity extends AppCompatActivity {
     private EditText etName1, etAddress1, etCode1, etCost1;
+
+    private Button btnPark1, btnPark2 , btnPark3;
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
 
 
     @Override
@@ -25,10 +34,38 @@ public class ParkingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_parking);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        etCost1 = findViewById(R.id.etCost1);
-        etCode1 = findViewById(R.id.etCode1);
-        etName1 = findViewById(R.id.etName1);
-        etAddress1 = findViewById(R.id.etAddress1);
+        btnPark1 = findViewById(R.id.btnPark1);
+        btnPark2 = findViewById(R.id.btnPark2);
+        btnPark3 = findViewById(R.id.btnPark3);
+
+
+
+        btnPark1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ParkingActivity.this,AddParkActivity.class);
+                startActivity(i);
+
+            }
+        });
+        btnPark2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ParkingActivity.this,AddParkActivity.class);
+                startActivity(i);
+
+            }
+        });
+        btnPark3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ParkingActivity.this,AddParkActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
